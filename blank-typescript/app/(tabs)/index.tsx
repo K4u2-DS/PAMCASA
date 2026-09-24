dad7f263ca5f74d7135485f60fb3c9110573b087
K4u2-DS <kauadiscor@gmail.com>
2026-09-24 02:04:43 +0000

    Base

---
+++ blank-typescript/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import Exemplo1 from '../../exemplo/Exemplo1';
+import Exemplo2 from '../../exemplo/Exemplo2';
+import Exemplo3 from '../../exemplo/Exemplo3';
+import Exemplo4 from '../../exemplo/Exemplo4';
+import Exemplo5 from '../../exemplo/Exemplo5';
+
+export default function App() {
+  // Para testar outro exemplo, mude aqui (ex: <Exemplo1 />, <Exemplo2 />, etc.)
+  return (
+    <Exemplo5 />
+  );
+}
